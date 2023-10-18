2367bac74caa8c9f9800c862df3851f8b6396d50
Vlad <vlad.lemeshok@gmail.com>
2023-10-18 16:40:41 +0300

    Fixed

---
+++ src/components/BikePage/BikePage.js
@@ -0,0 +1,17 @@
+import  {Link} from "react-router-dom";
+import {useParams} from "react-router-dom";   // ID из URL
+import React from "react";
+
+export  default  function BikePage({bikes}){
+   const {id} = useParams()
+    const bikeID = Number(id);
+    const cardToShow = bikes.find((item) => item.id === bikeID);
+   return(
+       <div>
+           <h1>Bike info:</h1>
+               <p>Bike name {cardToShow.name}</p>
+               <p>Cost: {cardToShow.cost}</p>
+           <Link to="/"><p>Back</p></Link>
+       </div>
+   )
+}
